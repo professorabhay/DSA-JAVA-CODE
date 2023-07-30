3ae39a1e6bf19fb3a4e6c9659efc0bb38dbec097
professorabhay <abhayetwup123@gmail.com>
2023-07-30 08:29:19 +0530

    Day1

---
+++ dataTypes.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class dataTypes {
+    public static void main(String[] args) {
+        System.out.println("*\n**\n***\n****");
+        String name = "Abhay";
+        int a = 10;
+        int b = 10;
+        System.out.println(a*b);
+        b = 20;
+        Scanner in = new Scanner(System.in);
+        int c = in.nextInt();
+        name  = in.next();
+        name  = in.nextLine();
+
+    }
+}
